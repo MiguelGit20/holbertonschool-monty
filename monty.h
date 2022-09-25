af9cfe2abd86c70bc11ece1bc90b48c88fdb63ce
MiguelGit20 <5178@holbertonschool.com>
2022-09-24 20:02:27 -0700

    monty header update

---
+++ monty.h
@@ -0,0 +1,50 @@
+#ifndef MONTY
+#define MONTY
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <ctype.h>
+/**
+ * struct stack_s - doubly linked list representation of a stack (or queue)
+ * @n: integer
+ * @prev: points to the previous element of the stack (or queue)
+ * @next: points to the next element of the stack (or queue)
+ *
+ * Description: doubly linked list node structure
+ * for stack, queues, LIFO, FIFO
+ */
+typedef struct stack_s
+{
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
+} stack_t;
+
+/**
+ * struct instruction_s - opcode and its function
+ * @opcode: the opcode
+ * @f: function to handle the opcode
+ *
+ * Description: opcode and its function
+ * for stack, queues, LIFO, FIFO
+ */
+typedef struct instruction_s
+{
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
+} instruction_t;
+
+void get_opc(char *opcode, stack_t **cp_stack, unsigned int line);
+void _free(stack_t *head);
+void _push(char *opcode, stack_t **top_stack, unsigned int line);
+void _pall(stack_t **sstack, unsigned int __attribute__((unused)) line);
+int _isnumber(char *opcode);
+void get_code(FILE *file, stack_t **tpstack);
+void _pint(stack_t **sstack, unsigned int __attribute((unused)) line);
+void _pop(stack_t **stack, unsigned int line);
+void _swap(stack_t **stack, unsigned int line);
+void _add(stack_t **stack, unsigned int line);
+void _nop(stack_t **stack, unsigned int line);
+
+#endif
